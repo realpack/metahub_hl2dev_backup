9dfdfe5cdba310f7179dedebf1d3191aa54552b7
server <server@100-183.static.spheral.ru>
2019-04-11 14:45:42 -0400

    first commit 2

---
+++ garrysmod/lua/autorun/server/sv_workshop_load.lua
@@ -0,0 +1,35 @@
+hook.Add( "Initialize", "WorkshopLoad_Initialize", function()
+	for k, v in pairs( engine.GetAddons() ) do
+		local _file = v.wsid and v.wsid or string.gsub( tostring( v.file ), "%D", "" )
+		resource.AddWorkshop( _file )
+	end
+end )
+
+maplist = {}
+
+-- maplist["rp_lusankya_metahub_v1"] = "1613799094"
+maplist["rp_city24_v2"] = "1542655022"
+maplist["rp_c18_updated"] = "1318848337"
+-- maplist["rp_hanamura"] = "1392013140"
+-- maplist["event_cdx_dathbase"] = "969977945"
+-- maplist["event_cdx_dathcanyon"] = "969977945"
+-- maplist["rp_venator_providence_battle"] = "804649089"
+-- maplist["rp_deathstar"] = "1598263759"
+-- maplist["rp_umbara"] = "1556357225"
+-- maplist["gm_underground_v"] = "1568719631"
+--add more maps here
+
+resource.AddWorkshop( "1624787752" )
+
+local map = game.GetMap() -- Get's the current map name
+local workshopid = maplist[map] 
+-- Finds the workshop ID for the current map name from the table above
+
+if( workshopid != nil )then
+	--If the map is in the table above, add it through workshop
+	print( "[WORKSHOP] Setting up maps. " ..map.. " workshop ID: " ..workshopid )
+	resource.AddWorkshop( workshopid )
+else
+	--If not, ) then hope the server has FastDL or the client has the map
+	print( "[WORKSHOP] Not available for current map. Using FastDL instead hopefully..." )
+end
